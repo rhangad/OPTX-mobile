1d4b928b3ab2d9cc231bec41f61a39f7265b77f2
lemon-bsnman <rparaguas@lemontechsolutions.com>
2019-11-23 04:24:07 +0800

    initial for device info and local db

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_app/cells/tableview_cell.dart';
+import 'package:my_app/utils/device_info.dart';
+import 'package:my_app/widgets/collapsible.dart';
 
-class Dashboard extends StatelessWidget {
-  // final String data;
+class Dashboard extends StatefulWidget {
 
-  // Dashboard({
-  //   Key key,
-  //   @required this.data,
-  // }) : super(key: key);
+  static final _deviceInfo = DeviceInfo();
+
+  @override
+  _DashboardState createState() => _DashboardState(deviceInfo: _deviceInfo);
+}
+
+class _DashboardState extends State<Dashboard> {
+
+  Map<String, dynamic> deviceData;
+
+  DeviceInfo deviceInfo;
+
+  _DashboardState({ this.deviceInfo });
+
+  @override
+  void initState() { 
+    super.initState();
+    
+    this.getDeviceInfo();
+  }
+
+  void getDeviceInfo() async {
+    final data = await deviceInfo.getData();
+
+    setState(() {
+      deviceData = data;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -20,33 +46,45 @@ class Dashboard extends StatelessWidget {
       body: Center(
           child: Column(
         children: <Widget>[
-          // Padding(
-          //   padding: EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 16),
-          //   child: Table(
-          //     children: [
-          //       TableRow(
-          //         children: [TableViewCell()],
-          //       ),
-          //       TableRow(
-          //         children: [TableViewCell()],
-          //       ),
-          //       TableRow(
-          //         children: [TableViewCell()],
-          //       ),
-          //       TableRow(
-          //         children: [TableViewCell()],
-          //       ),
-          //       TableRow(
-          //         children: [TableViewCell()],
-          //       ),
-          //       TableRow(
-          //         children: [TableViewCell()],
-          //       )
-          //     ],
-          //   ),
-          // )
+          Text("Device Information", style: TextStyle(fontSize: 18),),
+
+          // Collapsible(
+          //   title: "General",
+          //   children: <Widget>[
+          //     listItem("Account Number:", "11781"),
+          //     listItem("Name:", "Juan Dela Cruz"),
+          //     listItem("Birthday:", "December 31, 1990"),
+          //     listItem("Last Play Date:", "January 1, 1991"),
+          //     listItem("Host:", "None"),
+          //     listItem("Last Contact:", "None"),
+          //     Text(deviceData.toString()),
+          //     SizedBox(
+          //       height: 20,
+          //     )
+          //   ],
+          // ),
         ],
       )),
     );
   }
+
+  Widget listItem(key, value) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(key),
+          Text(value),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> itemFromDeviceInfo(Map<String, dynamic> deviceData) {
+    return deviceData
+      .map((key, value) => {
+        return listItem(key, value)
+      })
+  } 
 }
